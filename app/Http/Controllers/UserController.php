20c6ce96d46383467ab12b6ff4215da9426db3d9
Stid <stid.cardoso@trinitip.com>
2017-11-07 22:33:06 -0500

    initia commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+
+	public function index() {
+		$users = User::all()->toArray();
+
+		return response()->json($users);
+	}
+
+    public function show($id){
+    	//return User::find($id);
+    }    
+}

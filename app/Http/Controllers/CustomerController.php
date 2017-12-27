4fc1dd6358a78d5085e13a90f318106de2bce9d0
sgymo <sgymoapp@gmail.com>
2017-12-27 00:33:53 -0500

    update part 1

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -74,4 +74,48 @@ return response()->json($customers);
        //customer::create($request->all());
    }
 
+  public function update($gym, $customer, Request $request)
+   {
+      $valid = Validator::make($request->all(),[
+           'name1' => 'required',
+           'last_name1' => 'required',
+           'born' => 'required',
+           'address' => 'required',
+           'gender' => 'required',
+           'email' => 'required',
+           'phone' => 'required',
+           ]);
+       if ($valid->fails()) {
+           return response()->json([
+               "code" => 400,
+               "error" => "badRequest",
+           ],400);
+           exit();
+       }
+       
+
+       $customer = Customer::where('gym_id',$gym)->where('id',$customer)->get();
+       // $customer->gym_id = $gym;
+       $customer->card_id = $request->get('card_id');
+       $customer->finger = $request->get('finger');
+       $customer->name1 = $request->get('name1');
+       $customer->name2 = $request->get('name2');
+       $customer->last_name1 = $request->get('last_name1');
+       $customer->last_name2 = $request->get('last_name2');
+       $customer->born = $request->get('born');
+       $customer->address = $request->get('address');
+       $customer->gender = $request->get('gender');
+       $customer->email = $request->get('email');
+       $customer->phone = $request->get('phone');
+       $customer->user_login = $request->get('user_login');
+       // $customer->id = rand();
+       // $customer->gym_id = $id;
+       $customer->save();
+
+       return response()->json([
+           'code' => '201',
+           'response' => 'Create',
+       ], 201);
+   } 
+
 }
